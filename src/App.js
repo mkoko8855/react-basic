0c97aa96c2efbaaff0cb8b3798dbbc38d0a4f6c0
mkoko8855 <mkoko8855@naver.com>
2023-06-12 12:09:56 +0900

    20230612 리액트시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'; 에러뜨는거 신경쓰이면 이거 주석처리
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    //일단 다 밀어버리고
+    <h1>리액트 초보입니다.</h1>
   );
 }
 
-export default App;
+export default App; //App이라는 함수 자체를 export한다. 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App /> //app.js에있는 함수 자체가 여기로 리턴이 되는 것이다. 여기서 app을부른다. 우린 <h1>리액트 초보입니다. 를 적었었지. 이 app함수를 호출하고, 호출하면 리액트 초보입니다 라는 코드가 title = document.createElement('h1');으로 변환이 되서, title.textContent('react~~~~')로 되서 실행이 된다.
 );
 
 // If you want to start measuring performance in your app, pass a function
